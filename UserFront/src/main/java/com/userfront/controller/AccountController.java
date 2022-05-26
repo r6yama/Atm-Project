ff57099f1625442e5bc5c575b45c8cd28d4b7d53
Perez-Morales <linny.perez-morales@capgemini.com>
2022-05-26 17:00:36 -0400

    adding deposit

--- UserFront/src/main/java/com/userfront/controller/AccountController.java
+++ UserFront/src/main/java/com/userfront/controller/AccountController.java
@@ -1,7 +1,19 @@
 package com.userfront.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.userfront.domain.PrimaryAccount;
+import com.userfront.domain.SavingsAccount;
+import com.userfront.domain.User;
+import com.userfront.service.AccountService;
+import com.userfront.service.UserService;
 
 
 
@@ -9,18 +21,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/account")
 public class AccountController {
 
-@RequestMapping("/primaryAccount")
-public String primaryAccount() {
-return "primaryAccount";
-}
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private AccountService accountService;
+
+	@RequestMapping("/primaryAccount")
+	public String primaryAccount(Model model, Principal principal) {
+		User user = userService.findByUsername(principal.getName());
+		PrimaryAccount primaryAccount = user.getPrimaryAccount();
+
+		model.addAttribute("primaryAccount", primaryAccount);
+
+		return "primaryAccount";
+	}
 
 
+	@RequestMapping("/savingsAccount")
+	public String savingsAccount(Model model, Principal principal) {
+		User user = userService.findByUsername(principal.getName());
+		SavingsAccount savingsAccount = user.getSavingsAccount();
 
-@RequestMapping("/savingsAccount")
-public String savingsAccount() {
-return "primaryAccount";
-}
+		model.addAttribute("savingsAccount", savingsAccount);
 
+		return "savingsAccount";
+	}
+	
+	@RequestMapping(value = "/deposit", method = RequestMethod.GET)
+	public String deposit(Model model) {
+		model.addAttribute("accountType", "");
+		model.addAttribute("amount", "");
+		
+		return "deposit";
+	}
+	
+	@RequestMapping(value = "/deposit", method = RequestMethod.POST)
+	public String depositPOST(@ModelAttribute("amount") String amount, @ModelAttribute("accountType") String accountType, Principal principal) {
+			accountService.deposit(accountType, Double.parseDouble(amount), principal);
+			
+			return "redirect:/userFront";
 
+	}
 
 }
--- UserFront/src/main/java/com/userfront/service/AccountService.java
+++ UserFront/src/main/java/com/userfront/service/AccountService.java
@@ -1,11 +1,14 @@
 package com.userfront.service;
 
+import java.security.Principal;
+
 import com.userfront.domain.PrimaryAccount;
 import com.userfront.domain.SavingsAccount;
 
 public interface AccountService {
 	PrimaryAccount createPrimaryAccount();
 	SavingsAccount createSavingsAccount();
+	void deposit(String accountType, double amount, Principal principal);
 	
 	
 }
